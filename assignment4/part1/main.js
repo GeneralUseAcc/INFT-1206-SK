ec80f43969d22d7dc61a2a8ec960b0380febe4db
GeneralUseAcc <91762522+GeneralUseAcc@users.noreply.github.com>
2024-03-19 20:22:35 -0400

    step 1

--- assignment4/part1/main.js
+++ assignment4/part1/main.js
@@ -1,2 +1,11 @@
 // alert("Running Test")
 // console.log("Console Log Test")
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
